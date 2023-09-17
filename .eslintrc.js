7e4a44fc56b5d9bf3d2b33eaba54c7f11890b5cd
Matt Trueblood <dumdev13@gmail.com>
2023-09-17 17:08:17 -0400

    Initial setup with eslint, prettier, tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
+};
