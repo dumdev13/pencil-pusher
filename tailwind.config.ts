590a89a47151fb950eafcd8280baad1f719200e1
Matt Trueblood <dumdev13@gmail.com>
2023-09-17 17:53:08 -0400

    Adds Header component plus more eslint, prettier, tailwind stuff

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
